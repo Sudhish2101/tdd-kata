40920e7695dc936663ab5f9ab97e858185612ed8
sudhish <sudhish.mishra@gromo.in>
2024-06-08 18:28:55 +0530

    added api for calculating the string and returning the result

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
+import { CalculateStringDto } from './dto/sum-numbers.dto';
 
-@Controller()
+@Controller('/api/')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('sum')
+  async calculateString(@Body() calculateStringDto: CalculateStringDto, @Res() res: Response) {
+    try {
+      let data = await this.appService.calculateString(calculateStringDto);
+      console.log(data)
+      return res.status(HttpStatus.OK).json({
+        success: true, result: data
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(HttpStatus.OK).json({
+        success: true, result: err.message
+      });
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { CalculateStringDto } from './dto/sum-numbers.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async calculateString(calculateStringDto: CalculateStringDto): Promise<number> {
+    let numbers = calculateStringDto.numbers;
+    // If the input string is empty, return 0
+    if (!numbers) {
+      return 0;
+    }
+
+    let delimiter = /[,\n]/; // Default delimiter pattern (comma or newline)
+    let customDelimiter = false;
+
+    // Check if there's a custom delimiter
+    if (numbers.startsWith("//")) {
+      const delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
+      numbers = numbers.substring(delimiterEndIndex + 1);
+      customDelimiter = true;
+    }
+
+    // Split the input string by the delimiter(s)
+    const numList = numbers.split(delimiter);
+
+    let total = 0;
+    const negativeNumbers: number[] = [];
+
+    // Sum up the numbers and check for negatives
+    for (const num of numList) {
+      if (num) {
+        const n = parseInt(num, 10);
+        if (n < 0) {
+          negativeNumbers.push(n);
+        }
+        total += n;
+      }
+    }
+
+    // If there are negative numbers, throw an exception
+    if (negativeNumbers.length > 0) {
+      throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(", ")}`);
+    }
+    return total;
   }
 }
